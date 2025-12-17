df3a3cfe8223c387cd4f863fa5bd59b1c6df54d3
chtang <chtang@hmc.edu>
2025-12-17 20:21:32 +0700

    feat(cost): added general lp distance metrics

---
+++ core/cost/lpnorm.py
@@ -0,0 +1,94 @@
+"""Define Euclidean cost metrics for two features. Written with optimized numpy and numba""" #TODO: write tests for this file
+
+# library imports
+import numpy as np
+from numba import njit
+
+# custom imports
+from .cost_metric import CostMetric
+
+
+# define v2v cost function
+@njit
+def lp_dist_vec2vec(fv_1: np.ndarray, fv_2: np.ndarray, p: int):
+    r"""Calculates lp norm distance between two feature frames fv_1 and fv_2.
+    The lp norm is defined as:
+
+    .. math::
+        d(fv_1, fv_2) = \left( \sum_{i=1}^{n} |fv_1[i] - fv_2[i]|^p \right)^{1/p}
+
+    where :math:`n` is the dimension of the feature frames and :math:`p` is the dimension of the norm.
+
+    Args:
+        fv_1 (np.ndarray): reference feature frame, shape (n_features, 1)
+        fv_2 (np.ndarray): query feature frame, shape (n_features, 1)
+        p (int): dimension of the norm.
+
+    Returns:
+        lp norm distance between fv_1 and fv_2
+    """
+
+    diff = fv_1 - fv_2
+    return np.power(np.sum(np.power(diff, p)), 1 / p)
+
+
+class LpNormDistance(CostMetric):
+    """Class for calculating lp norm distance between feature vectors/matrices."""
+
+    def __init__(self, p: int):
+        super().__init__(v2v_cost=lp_dist_vec2vec, name=f"l{p}-norm")
+        self.p = p
+
+    ### Matrix-Matrix Lp Norm Distance
+    def mat2mat(self, fm_1: np.ndarray, fm_2: np.ndarray):
+        """Calculates lp norm distance between two feature matrices fm_1 and fm_2.
+
+        Optimized implementation using broadcasting and matrix operations.
+
+        Args:
+            fm_1: Reference feature matrix, shape (n_features, n_frames_1)
+            fm_2: Query feature matrix, shape (n_features, n_frames_2)
+
+        Returns:
+            lp norm distance matrix, shape (n_frames_1, n_frames_2).
+            Element (i, j) is the lp norm distance between fm_1[:, i] and fm_2[:, j].
+        """
+        # Use broadcasting: (n_features, n_frames_1, 1) - (n_features, 1, n_frames_2)
+        # Results in (n_features, n_frames_1, n_frames_2)
+        diff = fm_1[:, :, np.newaxis] - fm_2[:, np.newaxis, :]
+
+        # Sum over features axis and return (n_frames_1, n_frames_2)
+        return np.power(np.sum(np.power(diff, self.p), axis=0), 1 / self.p)
+
+    ### Vector-Matrix Lp Norm Distance
+    def mat2vec(self, fm_1: np.ndarray, fv_2: np.ndarray):
+        """Calculates lp norm distance between a feature matrix fm_1 and a feature frame vector fv_2.
+
+        Optimized implementation using broadcasting.
+
+        Args:
+            fm_1: Reference feature matrix, shape (n_features, n_frames)
+            fv_2: Query feature frame, shape (n_features, 1)
+
+        Returns:
+            lp norm distance vector, shape (n_frames,).
+            Element i is the lp norm distance between fm_1[:, i] and fv_2.
+        """
+        # Broadcast subtraction: (n_features, n_frames) - (n_features, 1)
+        diff = fm_1 - fv_2
+
+        # Sum over features axis
+        return np.power(np.sum(np.power(diff, self.p), axis=0), 1 / self.p)
+
+    ### Vector-Vector Lp Norm Distance
+    def vec2vec(self, fv_1: np.ndarray, fv_2: np.ndarray):
+        """Calculates lp norm distance between two feature frame vectors fv_1 and fv_2.
+
+        Args:
+            fv_1: Reference feature frame, shape (n_features, 1)
+            fv_2: Query feature frame, shape (n_features, 1)
+
+        Returns:
+            lp norm distance (scalar) between fv_1 and fv_2
+        """
+        return self.v2v_cost(fv_1, fv_2, self.p)
--- core/cost/manhattan.py
+++ core/cost/manhattan.py
@@ -1,4 +1,4 @@
-"""Define Euclidean cost metrics for two features. Written with optimized numpy and numba"""  # TODO: write tests for this file
+"""Define Manhattan cost metrics for two features. Written with optimized numpy and numba""" #TODO: write tests for this file
 
 # library imports
 import numpy as np
@@ -10,32 +10,32 @@ from .lpnorm import LpNormDistance
 
 # define v2v cost function
 @njit
-def euclidean_dist_vec2vec(fv_1: np.ndarray, fv_2: np.ndarray):
-    """Calculates euclidean distance between two feature frames fv_1 and fv_2.
+def manhattan_dist_vec2vec(fv_1: np.ndarray, fv_2: np.ndarray):
+    """Calculates manhattan distance between two feature frames fv_1 and fv_2.
 
     Args:
         fv_1 (np.ndarray): reference feature frame, shape (n_features, 1)
         fv_2 (np.ndarray): query feature frame, shape (n_features, 1)
 
     Returns:
-        Euclidean distance between fv_1 and fv_2
+        Manhattan distance between fv_1 and fv_2
     """
 
     diff = fv_1 - fv_2
-    return np.sqrt(np.sum(diff**2))
+    return np.sum(np.abs(diff))
 
 
-class EuclideanDistance(LpNormDistance):
-    """Class for calculating Euclidean distance between feature vectors/matrices."""
+class ManhattanDistance(LpNormDistance):
+    """Class for calculating Manhattan distance between feature vectors/matrices."""
 
     def __init__(self):
         super().__init__(p=2)
-        self.v2v_cost = euclidean_dist_vec2vec
-        self.name = "euclidean"
+        self.v2v_cost = manhattan_dist_vec2vec
+        self.name = "manhattan"
 
-    ### Matrix-Matrix Euclidean Distance
+    ### Matrix-Matrix Manhattan Distance
     def mat2mat(self, fm_1: np.ndarray, fm_2: np.ndarray):
-        """Calculates Euclidean distance between two feature matrices fm_1 and fm_2.
+        """Calculates Manhattan distance between two feature matrices fm_1 and fm_2.
 
         Optimized implementation using broadcasting and matrix operations.
 
@@ -44,19 +44,19 @@ class EuclideanDistance(LpNormDistance):
             fm_2: Query feature matrix, shape (n_features, n_frames_2)
 
         Returns:
-            Euclidean distance matrix, shape (n_frames_1, n_frames_2).
-            Element (i, j) is the Euclidean distance between fm_1[:, i] and fm_2[:, j].
+            Manhattan distance matrix, shape (n_frames_1, n_frames_2).
+            Element (i, j) is the Manhattan distance between fm_1[:, i] and fm_2[:, j].
         """
         # Use broadcasting: (n_features, n_frames_1, 1) - (n_features, 1, n_frames_2)
         # Results in (n_features, n_frames_1, n_frames_2)
         diff = fm_1[:, :, np.newaxis] - fm_2[:, np.newaxis, :]
 
         # Sum over features axis and return (n_frames_1, n_frames_2)
-        return np.sqrt(np.sum(diff**2, axis=0))
+        return np.sum(np.abs(diff), axis=0)
 
-    ### Vector-Matrix Euclidean Distance
+    ### Vector-Matrix Manhattan Distance
     def mat2vec(self, fm_1: np.ndarray, fv_2: np.ndarray):
-        """Calculates Euclidean distance between a feature matrix fm_1 and a feature frame vector fv_2.
+        """Calculates Manhattan distance between a feature matrix fm_1 and a feature frame vector fv_2.
 
         Optimized implementation using broadcasting.
 
@@ -65,24 +65,24 @@ class EuclideanDistance(LpNormDistance):
             fv_2: Query feature frame, shape (n_features, 1)
 
         Returns:
-            Euclidean distance vector, shape (n_frames,).
-            Element i is the Euclidean distance between fm_1[:, i] and fv_2.
+            Manhattan distance vector, shape (n_frames,).
+            Element i is the Manhattan distance between fm_1[:, i] and fv_2.
         """
         # Broadcast subtraction: (n_features, n_frames) - (n_features, 1)
         diff = fm_1 - fv_2
 
         # Sum over features axis
-        return np.sqrt(np.sum(diff**2, axis=0))
+        return np.sum(np.abs(diff), axis=0)
 
-    ### Vector-Vector Euclidean Distance
+    ### Vector-Vector Manhattan Distance
     def vec2vec(self, fv_1: np.ndarray, fv_2: np.ndarray):
-        """Calculates Euclidean distance between two feature frame vectors fv_1 and fv_2.
+        """Calculates Manhattan distance between two feature frame vectors fv_1 and fv_2.
 
         Args:
             fv_1: Reference feature frame, shape (n_features, 1)
             fv_2: Query feature frame, shape (n_features, 1)
 
         Returns:
-            Euclidean distance (scalar) between fv_1 and fv_2
+            Manhattan distance (scalar) between fv_1 and fv_2
         """
         return self.v2v_cost(fv_1, fv_2)
